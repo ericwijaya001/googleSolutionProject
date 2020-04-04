8368e0b1b2e3e444d6f3628f9f11c8f1b05f1bb7
Eric <ericwijaya001@gmail.com>
2020-04-04 20:43:30 +0700

    moga udah bener

--- apiregister.py
+++ apiregister.py
@@ -1,20 +1,9 @@
 from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
 import json
+import hashlib
 
-# buat coba aja
-datainput = {
-        'username' : 'orginput1',
-        'password' : 'orginput1',
-        'is_login': '0', 
-        'fotoprofil' : 'orginput1.jpg',
-        'nama' : 'namaorginput1', 
-        'email' : 'orginput1@gmail.com', 
-        'telepon' : '0123456789',
-        'levelmember' : '0'
-        }
-
-app = Flask(__name__,template_folder='template')
+app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
@@ -22,33 +11,32 @@ app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'regisdb'
 
 mysql = MySQL(app)
+json_response = {}
 
 @app.route('/',methods=['GET','POST'])
 def home():
-    # username_input = request.json['username']
-    # password_input = request.json['password']
-    # is_login = request.json['is_login']
-    # fotoprofil = request.json['fotoprofil']
-    # nama = request.json['nama']
-    # email = request.json['email']
-    # telepon = request.json['telepon']
-    # levelmember = request.json['levelmember']
+    username_input = request.json['data']['username']
+    password_input = request.json['data']['password']
+    is_login = request.json['data']['is_login']
+    foto_profil = request.json['data']['foto_profil']
+    nama = request.json['data']['nama']
+    email = request.json['data']['email']
+    telepon = request.json['data']['telepon']
+    level_member = request.json['data']['level_member']
 
-    username_input = datainput['username']
-    password_input = datainput['password']
-    is_login = datainput['is_login']
-    fotoprofil = datainput['fotoprofil']
-    nama = datainput['nama']
-    email = datainput['email']
-    telepon = datainput['telepon']
-    levelmember = datainput['levelmember']
+    # md5
+    hash_obj = hashlib.md5(password_input.encode())
+    md5_password = hash_obj.hexdigest()
 
     cur = mysql.connection.cursor()
-    query = "INSERT INTO `regis` (`id`, `username`, `password`, `is_login`, `fotoprofil`, `nama`, `email`, `telepon`, `levelmember`) VALUES ('2', '{}', '{}', {}, '{}', '{}', '{}', {}, {});".format(username_input,password_input,is_login,fotoprofil,nama,email,telepon,levelmember)
+    query = "INSERT INTO `regis` (`id`, `username`, `password`, `is_login`, `foto_profil`, `nama`, `email`, `telepon`, `level_member`) VALUES (NULL, '{}', '{}', {}, '{}', '{}', '{}', {}, {});".format(username_input,md5_password,is_login,foto_profil,nama,email,telepon,level_member)
     cur.execute(query)
     mysql.connection.commit()
     cur.close()
-    return render_template('home.html',mbo='NTAP')
+    
+    json_response['code'] = 0
+    json_response['message'] = 'success'
+    return json_response
 
 if __name__ == '__main__':
     app.run(debug=True)
--- regis.sql
+++ regis.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 04, 2020 at 06:48 AM
+-- Generation Time: Apr 04, 2020 at 03:42 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.1
 
@@ -33,19 +33,22 @@ CREATE TABLE `regis` (
   `username` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
   `is_login` int(2) NOT NULL,
-  `fotoprofil` varchar(50) NOT NULL,
+  `foto_profil` varchar(50) NOT NULL,
   `nama` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `telepon` int(50) NOT NULL,
-  `levelmember` int(10) NOT NULL
+  `level_member` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `regis`
 --
 
-INSERT INTO `regis` (`id`, `username`, `password`, `is_login`, `fotoprofil`, `nama`, `email`, `telepon`, `levelmember`) VALUES
-(2, 'orginput1', 'orginput1', 0, 'orginput1.jpg', 'namaorginput1', 'orginput1@gmail.com', 123456789, 0);
+INSERT INTO `regis` (`id`, `username`, `password`, `is_login`, `foto_profil`, `nama`, `email`, `telepon`, `level_member`) VALUES
+(2, 'orginput1', 'orginput1', 0, 'orginput1.jpg', 'namaorginput1', 'orginput1@gmail.com', 123456789, 0),
+(3, 'orginput2', 'orginput2', 0, 'orginput2.jpg', 'namaorginput2', 'orginput2@gmail.com', 223456789, 0),
+(4, 'orginput2', 'orginput2', 0, 'orginput2.jpg', 'namaorginput2', 'orginput2@gmail.com', 223456789, 0),
+(5, 'orginput2', '09849b7220814ca43eee25e3fba51e6f', 0, 'orginput2.jpg', 'namaorginput2', 'orginput2@gmail.com', 223456789, 0);
 
 --
 -- Indexes for dumped tables
@@ -65,7 +68,7 @@ ALTER TABLE `regis`
 -- AUTO_INCREMENT for table `regis`
 --
 ALTER TABLE `regis`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
