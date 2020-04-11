3ba7a8e2a782482b771727e52bba73996d8877bc
Eric <ericwijaya001@gmail.com>
2020-04-11 13:20:36 +0700

    moga bener v1.0

--- apigetlistbencana.py
+++ apigetlistbencana.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
 import json
 import hashlib
@@ -11,28 +11,29 @@ app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'userdb'
 
 mysql = MySQL(app)
-json_response = {}
 
 @app.route('/',methods=['GET','POST'])
 def home():
-    username = request.json['username']
-    cur = mysql.connection.cursor()
-    query = "SELECT username FROM `users` WHERE username ='{}'".format(username)
-    query_executed = cur.execute(query)
+    json_response = {}
 
-    if query_executed == 1:
-        query_get_data = "UPDATE `users` SET `isLogin` = '0' WHERE `username` = '{}';".format(username)
-        cur.execute(query_get_data)
-        mysql.connection.commit()
+    cur = mysql.connection.cursor()
+    query = "SELECT * FROM `listbencana`"
+    found = cur.execute(query)
+
+    # found > 0 brarti ada username n pass di db 
+    if found > 0:
+        row_headers=[x[0] for x in cur.description] #this will extract row headers
+        rv = cur.fetchall()
+        json_data=[]
+        for result in rv:
+            json_data.append(dict(zip(row_headers,result)))
+        json_response = json.loads(json.dumps(json_data[0]))
         json_response['code'] = 0
         json_response['message'] = 'success'
-        cur.close()
         return json_response
     json_response['code'] = 9999
-    json_response['message'] = 'failed'
-    cur.close()
+    json_response['message'] = "failed"
     return json_response
-    
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ listbencana.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 11, 2020 at 08:20 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `userdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `listbencana`
+--
+
+CREATE TABLE `listbencana` (
+  `disasterTitle` varchar(50) NOT NULL,
+  `description` varchar(50) NOT NULL,
+  `id` int(11) NOT NULL,
+  `disasterPhoto` varchar(50) NOT NULL,
+  `donationMethod` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='totalDonation';
+
+--
+-- Dumping data for table `listbencana`
+--
+
+INSERT INTO `listbencana` (`disasterTitle`, `description`, `id`, `disasterPhoto`, `donationMethod`) VALUES
+('bencanasatu', 'deskripsibencanasatu', 1, 'bencanasatu.jpg', 'transfer'),
+('bencanadua', 'deskripsibencanadua', 2, 'bencanadua.jpg', 'paypal');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `listbencana`
+--
+ALTER TABLE `listbencana`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `listbencana`
+--
+ALTER TABLE `listbencana`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
