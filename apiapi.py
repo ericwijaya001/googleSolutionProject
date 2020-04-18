fcdf7336e86a0f7fd5e725232a6485dc1c75268f
Eric <ericwijaya001@gmail.com>
2020-04-18 13:00:43 +0700

    moga udah bener :)

--- apiapi.py
+++ apiapi.py
@@ -27,16 +27,19 @@ def home():
 
     # found = 1 brarti ada username n pass di db 
     if found == 1:
-        querygetdata = "SELECT photo,name,email,telp,address,total FROM `users` WHERE username='{}' AND password='{}'".format(username,md5_password)
+        querygetdata = "SELECT username,photo,name,email,telp,address,total FROM `users` WHERE username='{}' AND password='{}'".format(username,md5_password)
         cur.execute(querygetdata)
         row_headers=[x[0] for x in cur.description] #this will extract row headers
         rv = cur.fetchall()
         json_data=[]
         for result in rv:
             json_data.append(dict(zip(row_headers,result)))
-        json_response = json.loads(json.dumps(json_data[0]))
+        json_response['data'] = json.loads(json.dumps(json_data[0]))
         json_response['code'] = 0
         json_response['message'] = 'success'
+        update_islogin = "UPDATE `users` SET `isLogin` = '1' WHERE username ='{}';".format(username)
+        cur.execute(update_islogin)
+        mysql.connection.commit()
         return json_response
     json_response['code'] = 9999
     json_response['message'] = "failed"
